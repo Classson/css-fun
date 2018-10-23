6a2135abee46700e44a142b0846e1d6a3220df34
Alex <Jillbk22@gmail.com>
2018-10-23 16:51:38 -0400

    set up smallestCommons

--- script.js
+++ script.js
@@ -187,20 +187,47 @@
 //
 //For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
 
-function addTogether(a, b) {
-    console.log(b);
-  if((typeof a === 'number') && (typeof b === 'number')){
-      return a+b;
-  } 
-    else if(typeof a === 'number' && typeof b === 'undefined'){
-        console.log('running');
-        return function(num){
-            if(typeof num === 'number')
-            return num + a;
-        }
-    }
+//function addTogether(a, b) {
+//    console.log(b);
+//  if((typeof a === 'number') && (typeof b === 'number')){
+//      return a+b;
+//  } 
+//    else if(typeof a === 'number' && typeof b === 'undefined'){
+//        console.log('running');
+//        return function(num){
+//            if(typeof num === 'number')
+//            return num + a;
+//        }
+//    }
+//
+//}
 
+//console.log(addTogether(2)([3]));
+
+//Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters.
+//
+//The range will be an array of two numbers that will not necessarily be in numerical order.
+//
+//For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is also evenly divisible by all numbers between 1 and 3. The answer here would be 6.
+
+function smallestCommons(arr) {
+    let range = [];
+    arr.sort((a,b) => a-b);
+    
+    for(let i = arr[0]; i <= arr[1]; i++){
+        range.push(i);
+    }
+    
+    let condition = true;
+    for(let i = arr[0]; i <=100; i++){
+        let testNum = i;
+        console.log("running");
+        for(let j = range[0]; j < range[range.length]; j++){
+            console.log(j);
+        }
+        condition = false;
+    }
+    
 }
 
-
-console.log(addTogether(2)([3]));
+smallestCommons([1, 5]);
