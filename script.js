05629f3ba1e6b537016ffd551b865e657ead64c2
Alex <Jillbk22@gmail.com>
2018-10-29 18:41:01 -0400

    solved busStops

--- script.js
+++ script.js
@@ -791,19 +791,33 @@
 //    return `${errors}/${s.length}`
 //}
 
-function comp(array1, array2){
-  if(!array1 || !array2){
-  return false}
-  if(array1.length !== array2.length){
-    return false;
-  }
-  array1.sort((a,b) => a-b);
-  array2.sort((a,b) => a-b);
-  for(let i = 0; i < array1.length; i++){
-      let squared = array1[i] * array1[i];
-      if(squared !== array2[i]){
-        return false;
-      }
+//function comp(array1, array2){
+//  if(!array1 || !array2){
+//  return false}
+//  if(array1.length !== array2.length){
+//    return false;
+//  }
+//  array1.sort((a,b) => a-b);
+//  array2.sort((a,b) => a-b);
+//  for(let i = 0; i < array1.length; i++){
+//      let squared = array1[i] * array1[i];
+//      if(squared !== array2[i]){
+//        return false;
+//      }
+//    }
+//    return true;
+//}
+
+//You are provided with a list (or array) of integer arrays (or tuples). Each integer array has two items which represent number of people get into bus (The first item) and number of people get off the bus (The second item) in a bus stop.
+//
+//Your task is to return number of people who are still in the bus after the last bus station (after the last array). Even though it is the last bus stop, the bus is not empty and some people are still in the bus, and they are probably sleeping there :D
+
+var number = function(busStops){
+    let count = 0;
+    for(let i = 0; i < busStops.length; i++){
+        count += busStops[i][0];
+        count -= busStops[i][1];
     }
-    return true;
+    
+    return count;
 }
