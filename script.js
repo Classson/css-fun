c1b7fdbd8d14d931034e78799f0f8b9418270919
Alex <Jillbk22@gmail.com>
2018-11-06 18:54:35 -0500

    added exact change outcome

--- script.js
+++ script.js
@@ -1392,16 +1392,19 @@ function checkCashRegister(price, cash, cid) {
         return sum;
     }
     let cashOnHand = cohFunc(cid).toFixed(2);
-    let changeNeeded = cash - price;
+    let changeNeeded = (cash - price).toFixed(2);
     if(cashOnHand < changeNeeded){
         return {status: "INSUFFICIENT_FUNDS", change: []};
     }
-    console.log(changeNeeded);
-  
+    if(cashOnHand === changeNeeded){
+        return {status: "CLOSED", change: cid}
+    }
+    let changeSplit = changeNeeded.split(".");
+    let dollarsNeeded = changeSplit[0];
+    let coinsNeeded = changeSplit[1];
+    console.log(coinsNeeded);
 }
 
-//checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
 
 
-
-console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
