01e6ddebe0e98f96ba59946ef18d409ce4f0e255
Alex <Jillbk22@gmail.com>
2018-11-17 17:54:50 -0500

    set up phoneAlgo

--- script.js
+++ script.js
@@ -1693,20 +1693,40 @@ var watchList = [
 
 //There is an array with some numbers. All numbers are equal except for one. Try to find it!
     
-function findUniq(arr) {
-    let arrObj = {};
-    for(let i = 0; i < arr.length; i++){
-        if (arrObj[arr[i]]){
-            arrObj[arr[i]]++;
-        } else {
-            arrObj[arr[i]] = 1;
-        }
-    }
-    for(num in arrObj){
-        if (arrObj[num] === 1){
-            return Number(num);
-        }
-    }
+//function findUniq(arr) {
+//    let arrObj = {};
+//    for(let i = 0; i < arr.length; i++){
+//        if (arrObj[arr[i]]){
+//            arrObj[arr[i]]++;
+//        } else {
+//            arrObj[arr[i]] = 1;
+//        }
+//    }
+//    for(num in arrObj){
+//        if (arrObj[num] === 1){
+//            return Number(num);
+//        }
+//    }
+//}
+//
+//console.log(findUniq([ 1, 1, 1, 2, 1, 1 ]));
+
+//John keeps a backup of his old personal phone book as a text file. On each line of the file he can find the phone number (formated as +X-abc-def-ghij where X stands for one or two digits), the corresponding name between < and > and the address.
+//
+//Unfortunately everything is mixed, things are not always in the same order, lines are cluttered with non-alpha-numeric characters.
+//
+//Examples of John's phone book lines:
+//
+//"/+1-541-754-3010 156 Alphand_St. <J Steeve>\n"
+//
+//" 133, Green, Rd. <E Kustur> NY-56423 ;+1-541-914-3010!\n"
+//
+//"<Anastasia> +48-421-674-8974 Via Quirinal Roma\n"
+//
+//Could you help John with a program that, given the lines of his phone book and a phone number returns a string for this number : "Phone => num, Name => name, Address => adress"
+
+function phone(strng, num) {
+    
 }
 
-console.log(findUniq([ 1, 1, 1, 2, 1, 1 ]));
+console.log();
