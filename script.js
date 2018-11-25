e82f42b9d58112f31163e00b37a3c7da07c1cbfe
Alex <Jillbk22@gmail.com>
2018-11-25 08:47:30 -0500

    solved outed

--- script.js
+++ script.js
@@ -1799,15 +1799,37 @@ var watchList = [
 //
 //Recently, Jonny has heard Polycarpus's new dubstep track, but since he isn't into modern music, he decided to find out what was the initial song that Polycarpus remixed. Help Jonny restore the original song.
 
-function songDecoder(song){
-    let wordsArr = song.split("WUB");
-    let realWords = [];
-    for(let i = 0; i < wordsArr.length; i++){
-        if(wordsArr[i]){
-            realWords.push(wordsArr[i]);
-        }
+//function songDecoder(song){
+//    let wordsArr = song.split("WUB");
+//    let realWords = [];
+//    for(let i = 0; i < wordsArr.length; i++){
+//        if(wordsArr[i]){
+//            realWords.push(wordsArr[i]);
+//        }
+//    }
+//    return realWords.join(' ').trim();
+//}
+//
+//console.log(songDecoder("AWUBWUBWUBBWUBWUBWUBC"));
+
+//Given an object (meet) containing team member names as keys, and their happiness rating out of 10 as the value, you need to assess the overall happiness rating of the group. If <= 5, return 'Get Out Now!'. Else return 'Nice Work Champ!'.
+//
+//Happiness rating will be total score / number of people in the room.
+//
+//Note that your boss is in the room (boss), their score is worth double it's face value (but they are still just one person!).
+
+function outed(meet, boss){
+    let happiness = 0;
+    for(person in meet){
+        if(boss === person){
+            happiness += meet[person] * 2;
+        } else {
+        happiness += meet[person];}
     }
-    return realWords.join(' ').trim();
+    if(happiness/Object.keys(meet).length <=5){
+        return 'Get Out Now!';
+    }
+    return 'Nice Work Champ!';
 }
 
-console.log(songDecoder("AWUBWUBWUBBWUBWUBWUBC"));
+console.log(outed({'tim':0, 'jim':2, 'randy':0, 'sandy':7, 'andy':0, 'katie':5, 'laura':1, 'saajid':2, 'alex':3, 'john':2, 'mr':0}, 'laura'));
